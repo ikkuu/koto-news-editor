1daac2ed0ca92ff6b7aff001ebfa15aafd8ab5cc
ikkuu <bonkisha@gmail.com>
2025-07-07 13:15:22 +0900

    scriptエラー修正2

--- docs/script_updated.js
+++ docs/script_updated.js
@@ -1,5 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const mediaFiles = ['001.mp4', '002.mp4', '003.mp4', '004.mp4'];
+  const mediaFiles = ['001.mp4', '002.mp4', '003.mp4', '004.mp4', '005.mp4',
+    '006.mp4', '007.mp4', '008.mp4', '009.mp4', '010.mp4',
+    '011.mp4', '012.mp4', '013.mp4', '014.mp4', '015.mp4',
+    '016.mp4', '017.mp4', '018.mp4', '019.mp4', '020.mp4',
+    '021.mp4', '022.mp4', '023.mp4'
+];
   const mediaPanel = document.getElementById('mediaPanel');
   const timelineTrack = document.querySelector('.clip-track');
   const zoomToggle = document.getElementById('zoom-toggle');
